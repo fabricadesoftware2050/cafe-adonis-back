543267fde6705ae82c464bd3d90c8ebeddf5b4d8
Christian Camilo Serna Gamez <christian.sernag@bam.com.gt>
2025-10-15 12:09:51 -0500

    Consume todo mundo y local

--- config/cors.ts
+++ config/cors.ts
@@ -8,7 +8,7 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: ['*'],
+  origin: ['https://cafe-nuxt-front.vercel.app','http://127.0.0.1:5500'],
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
   exposeHeaders: [],
